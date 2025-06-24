a5b69850555af3af9657fcf9acb7858a4c1d412e
imrozkhan214 <khan.imrozmae@gmail.com>
2025-06-24 14:29:49 +0800

    added summarizer - khan

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SummarizeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SummarizeController::class, 'index']);
+Route::post('/summarize', [SummarizeController::class, 'summarize']);
+Route::view('/tools-hub', 'hub');
 

b4f678e7c40c146a3ad2c3cbaebb4f53fe8728d4
mabeyy <jeanmaverickdelacruz@gmail.com>
2025-06-24 14:17:45 +0800

    added controller

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function(){
-    return view('leveler');
+    return view('welcome');
 });
 
 Route::get('/leveler', 'App\Http\Controllers\LevelerController@showForm');

6a463de3298c665d52a0b02e314c9147351831a7
mabeyy <jeanmaverickdelacruz@gmail.com>
2025-07-05 16:43:49 +0800

    working with chat_router mysql

---
+++ database/migrations/2025_07_05_074800_create_chat_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('message_id');
+            $table->enum('sender', ['ai', 'human']);
+            $table->text('topic');
+            $table->timestamps();
+
+            $table->foreign('message_id')->references('id')->on('sessions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_messages');
+    }
+}
